2b29df931fdf914687f6b515a9a533b1b315a509
Romario Gustave <romagustave10@gmail.com>
2019-11-08 00:45:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,7 @@ if phrase = "HELLO WORLD"
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
- if phrase = "HELLO WORLD"
-    puts "NO, NOT SINCE 1938!
+ if phrase = "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
+  
